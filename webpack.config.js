c5c96d55eb3cc600579a2ee9499d7ee04c0e925a
Pablo Fajardo <pablofajardoruano@gmail.com>
2022-04-15 14:40:47 -0500

    agregando nuevament 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-	mode: 'production',
-	devtool: 'inline-source-map',
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-					},
-				],
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					'css-loader',
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html',
-		}),
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
 
-		new MiniCssExtractPlugin({
-			filename: 'assets/[name].css',
-		}),
-
-	
-
-		new webpack.DefinePlugin({
-			'process.env': {
-				REACT_APP_CLIENT_ID: JSON.stringify(process.env.REACT_APP_CLIENT_ID),
-				API_KEY: JSON.stringify(process.env.API_KEY),
-			},
-		}),
-	],
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		historyApiFallback: true,
-		port: 3005,
-	},
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: false,
+    historyApiFallback: true,
+    port: 3000,
+  },
+  performance: { hints: false },
 };
